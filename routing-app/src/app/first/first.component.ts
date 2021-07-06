ad8ca118dcf1ced6a70108902649ee4105db78cc
Ezekiel Obhafuoso <ezekielobhafuoso@gmail.com>
2021-07-06 07:09:14 +0100

    Feature: Getting information (optional parameters in this case) from the route

--- routing-app/src/app/first/first.component.ts
+++ routing-app/src/app/first/first.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstComponent implements OnInit {
 
-  constructor() { }
+  name: string = '';
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+  ) {}
+
+  // Todo: How to add information to a route
+  ngOnInit() {
+    // http://localhost:4200/first-component?name=best
+    // best is retrived from the route
+    this.route.queryParams.subscribe(params => {
+      this.name = params['name'];
+    });
   }
 
 }
